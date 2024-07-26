db4f7ce6e40d51a2fe8dc54ef65ea4580d48296d
Sankhlavinay <vinaysankhla4233@gmail.com>
2024-07-27 01:40:21 +0530

    add authentication remove confirmable

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
         registrations: 'users/registrations',
         sessions: 'users/sessions',
-        passwords: 'users/passwords',
-        confirmations: 'users/confirmations'
+        passwords: 'users/passwords'
+        # ,
+        # confirmations: 'users/confirmations'
   }
 
   root 'home#index'
